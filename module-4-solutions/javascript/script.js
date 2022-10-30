d2d11d2f83576b3fd81f521ba53b8d25940c4ac0
Kabute Grace <kabutemuthoni@gmail.com>
2022-10-30 22:04:38 +0300

    main

---
+++ module-4-solutions/javascript/script.js
@@ -0,0 +1,38 @@
+(function() {
+    let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    (function(window) {
+        let helloSpeaker = {};
+        helloSpeaker.call = 'hello';
+
+        helloSpeaker.hello = function speak() {
+            console.log(helloSpeaker.call + ' ' + names[i]);
+        }
+        window.helloSpeaker = helloSpeaker;
+    })(window);
+
+
+    (function(window) {
+        let speakGoodbye = {};
+        speakGoodbye.call = 'goodbye';
+
+        speakGoodbye.hello = function speak() {
+            console.log(speakGoodbye.call + ' ' + names[i]);
+        }
+        window.speakGoodbye = speakGoodbye;
+    })(window);
+
+
+    for (var i = 0; i < names.length; i++) {
+
+
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+
+        if (firstLetter === 'j') {
+            speakGoodbye.hello();
+        } else {
+
+            helloSpeaker.hello();
+        }
+    };
+})();
